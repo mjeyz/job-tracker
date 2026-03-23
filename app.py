6449a38cfedc19914b3ce00a3defc22148a267f0
Mudasir <thisismjeyz@gmail.com>
2026-03-23 10:09:32 +0500

    set basic flask login

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-
+from flask_bootstrap import Bootstrap5
+from flask_login import login_required, login_user, logout_user, current_user, LoginManager, UserMixin
+import psycopg2
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+Bootstrap5(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = "13114asdad"
 
+
+
+class User(UserMixin):
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("auth/login.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("auth/register.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
