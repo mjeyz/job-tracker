1b4b16e068924a3116cb881bc1a9653338d2ab95
Mudasir <thisismjeyz@gmail.com>
2026-04-04 11:08:15 +0500

    fix home page ui

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session, flash
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_login import login_required, login_user, logout_user, current_user, LoginManager, UserMixin
 import psycopg2
